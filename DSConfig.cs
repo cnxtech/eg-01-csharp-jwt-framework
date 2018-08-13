23d1c4a9bbea3f7bed7d761f530a47c20fdba595
Shadi Massalha <shady.massalha@gmail.com>
2018-08-13 13:50:36 +0300

    Squashed 'common/' content from commit 0530c67
    git-subtree-dir: common
    git-subtree-split: 0530c67824bfb2d3265f674b94de42028e340665

---
+++ DSConfig.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Configuration;
+
+namespace eg_01_csharp_jwt
+{
+    internal class DSConfig
+    {
+        private const string CLIENT_ID = "DS_CLIENT_ID";
+        private const string IMPERSONATED_USER_GUID = "DS_IMPERSONATED_USER_GUID";
+        private const string TARGET_ACCOUNT_ID = "DS_TARGET_ACCOUNT_ID";
+        private const string SIGNER_1_EMAIL = "DS_SIGNER_1_EMAIL";
+        private const string SIGNER_1_NAME = "DS_SIGNER_1_NAME";
+        private const string CC_1_EMAIL = "DS_CC_1_EMAIL";
+        private const string CC_1_NAME = "DS_CC_1_NAME";
+        private const string PRIVATE_KEY_FILE = "DS_PRIVATE_KEY_FILE";
+        private const string PRIVATE_KEY = "DS_RPIVATE_KEY";
+
+        static DSConfig()
+        {
+            ClientID = GetSetting(CLIENT_ID);
+            ImpersonatedUserGuid = GetSetting(IMPERSONATED_USER_GUID);
+            TargetAccountID = GetSetting(TARGET_ACCOUNT_ID);
+            OAuthRedirectURI = "https://www.docusign.com";
+            Signer1Email = GetSetting(SIGNER_1_EMAIL);
+            Signer1Name = GetSetting(SIGNER_1_NAME);
+            Cc1Email = GetSetting(CC_1_EMAIL);
+            Cc1Name = GetSetting(CC_1_NAME);
+            PrivateKeyFile = GetSetting(PRIVATE_KEY_FILE);
+            PrivateKey = GetSetting(PRIVATE_KEY);
+            AuthenticationURL = "https://account-d.docusign.com";
+            AUD = "account-d.docusign.com";
+            API = "restapi/v2";
+            PermissionScopes = "signature impersonation";
+            JWTScope = "signature";
+        }
+
+        private static string GetSetting(string configKey)
+        {
+            string val = Environment.GetEnvironmentVariable(configKey)
+                ?? ConfigurationManager.AppSettings.Get(configKey);
+
+            if ((PRIVATE_KEY_FILE.Equals(configKey) || PRIVATE_KEY.Equals(configKey)) && "FALSE".Equals(val))
+                return null;
+
+            return val ?? "";
+        }
+
+        public static string ClientID { get; private set; }
+        public static string ImpersonatedUserGuid { get; private set; }
+        public static string TargetAccountID { get; private set; }
+        public static string OAuthRedirectURI { get; private set; }
+        public static string Signer1Email { get; private set; }
+        public static string Signer1Name { get; private set; }
+        public static string Cc1Email { get; private set; }
+        public static string Cc1Name { get; private set; }
+        public static string PrivateKeyFile { get; private set; }
+        public static string PrivateKey { get; private set; }
+        public static string AuthenticationURL { get; private set; }
+        public static string AUD { get; private set; }
+        public static string API { get; private set; }
+        public static string PermissionScopes { get; private set;}
+        public static string JWTScope { get; private set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using DocuSign.eSign.Client;
+using DocuSign.eSign.Model;
+using System;
+using System.Collections.Generic;
+
+namespace eg_01_csharp_jwt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var apiClient = new ApiClient();
+
+            EnvelopeSummary result = new SendEnvelope(apiClient).Send();
+            Console.WriteLine("Envelope status: {0}. Envelope ID: {1}", result.Status, result.EnvelopeId);
+
+            Console.WriteLine("\nList envelopes in the account...");
+            EnvelopesInformation envelopesList = new ListEnvelopes(apiClient).List();
+
+            List<Envelope> envelopes = envelopesList.Envelopes;
+
+            if (envelopesList != null && envelopes.Count > 2)
+            {
+                //Console.WriteLine("Results for {0} envelopes were returned. Showing the first two: ", envelopes.Count);
+                envelopesList.Envelopes = new List<Envelope>()
+                {
+                    envelopes[0],
+                    envelopes[1]
+                };
+            }
+
+            DSHelper.PrintPrettyJSON(envelopesList);
+
+            Console.WriteLine("Done. Hit enter to exit...");
+            Console.ReadKey();
+        }
+    }
+}
